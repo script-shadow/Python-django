d7a0df1cecc51174663300049b0ad533e1b1f9e3
MontessoriVisualization <MontessoriVisualization@users.noreply.github.com>
2026-03-23 22:26:02 +0545

    chore:task1.js

---
+++ getStarted/js/task1.js
@@ -0,0 +1,9 @@
+const walletBallance=5000
+
+const cartItems=[500,1200,350]
+cartItems.push(2000)
+cartItems.pop()
+const recomendedItems=[100,200]
+const inputCopenCode="DisCOunT10"
+const couponCode = 'DISCOUNT10'
+const input= prompt("hello")
